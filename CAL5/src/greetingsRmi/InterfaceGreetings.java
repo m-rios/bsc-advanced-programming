0362b4541413d5979a88cf0ff0986494429b1258
Mario <mrm994@gmail.com>
2014-04-09 17:44:25 +0200

    removed session10 related packages

--- CAL5/src/greetingsRmi/InterfaceGreetings.java
+++
@@ -1,9 +0,0 @@
-package greetingsRmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface InterfaceGreetings extends Remote {
-
-    String greet(String nombre) throws RemoteException;
-}
